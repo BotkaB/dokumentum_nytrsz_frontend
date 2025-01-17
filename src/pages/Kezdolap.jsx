088c725e34d5ffc2bbe63860cae5d741308a58c0
BotkaB <botikabinaka@gmail.com>
2025-01-17 19:20:16 +0100

    kezdőlap háttérképhez igazítása.

--- src/pages/Kezdolap.jsx
+++ src/pages/Kezdolap.jsx
@@ -4,30 +4,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function Kezdolap() {
-  const { user} = useAuthContext(); 
+  const { user } = useAuthContext(); 
 
   return (
-    <div className="container-fluid" style={{
-      backgroundImage: 'url("/Kezdooldal_hatterkep.jpg")',
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center center',
-      height: '100vh',
-      margin: 0
-  }}>
-      
-     
-      
+    <article className="container-fluid" style={articleStyle}>
       {user ? (
         <>
-          <p>Kedves: {user.name}</p>
-          <p>Jogosultságod: {user.role === 0 ? "Admin" : user.role === 1 ? "Dokumentumellenőrző" : user.role === 2?"Statisztikai lekérdező":"Ismeretlen szerep"}</p>
-
+          <p style={pStyle}>Kedves: {user.name}</p>
+          <p style={pStyle}>Jogosultságod: {user.role === 0 ? "Admin" : user.role === 1 ? "Dokumentumellenőrző" : user.role === 2 ? "Statisztikai lekérdező" : "Ismeretlen szerep"}</p>
         </>
       ) : (
-        <p>Kedves Látogató!</p>
+        <p style={pStyle}>Kedves Látogató!</p>
       )}
-    </div>
+    </article>
   );
 }
+
+const articleStyle = {
+  backgroundImage: 'url("/Kezdooldal_hatterkep.jpg")',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center center',
+  height: '100vh',
+  margin: 0,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'flex-start',
+  paddingTop: '30vh' // Kicsit fentebb hozva a p tageket az article egyharmadánál
+};
+
+const pStyle = {
+  margin: '0 0 1rem 0', // Például szép stílusok
+  fontSize: '1.2rem'
+};
+
 

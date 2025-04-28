b25861811483acf0751e3385f3a9ea5d5a4c5e24
BotkaB <botikabinaka@gmail.com>
2025-04-28 22:45:11 +0200

    nav véglegesítés

--- src/components/DokumentumNav.jsx
+++ src/components/DokumentumNav.jsx
@@ -5,10 +5,6 @@ export default function DokumentumNav() {
     return (
         <NavDropdown title="Dokumentumok" id="basic-nav-dropdown">
              <NavDropdown.Item as={Link} to="/documents/ugyfelek">Ügyfelek</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
         </NavDropdown>
     )
 }
